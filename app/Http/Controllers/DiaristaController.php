4c4378dd6fe3663cab84b8686cd213c07fd838a5
DiegoSousaSilva <derickbenji2@gmail.com.br>
2021-10-26 23:47:30 -0300

    Criando integracao com banco de dados

---
+++ app/Http/Controllers/DiaristaController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Diarista;
+use Illuminate\Http\Request;
+
+class DiaristaController extends Controller
+{
+    public function index()
+    {
+        $diaristas = Diarista::get();
+        return view('index', [
+            'diaristas' => $diaristas
+        ]);
+}
+}
